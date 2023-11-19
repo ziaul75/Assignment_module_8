08d5d255bff92ddaa9cc63818ddc6add0cf9ebca
ziaul75 <ziaulchowdhury75@gmail.com>
2023-11-19 18:21:26 +0600

    module-8

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $cookie = Cookie::make($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response()->json($data)->cookie($cookie);
+    }
+}
